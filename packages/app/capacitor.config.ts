0bd3e82108e0ebfb5e11547e513665622644ad52
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 22:51:57 +0700

    update cap

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
 	appId: 'io.github.hieudoanm.blackjack',
-	appName: '@blackjack/frontend',
+	appName: 'Blackjack',
 	webDir: '../../docs',
 	android: { path: 'mobile/android' },
 	ios: { path: 'mobile/ios' },
